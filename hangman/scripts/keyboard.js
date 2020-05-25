6c4c1ea139feccde6be27cfb8e9b4967c1235c74
giraldiego <giraldiego@gmail.com>
2020-05-24 23:52:54 -0500

    more responsive

--- hangman/scripts/keyboard.js
+++ hangman/scripts/keyboard.js
@@ -1,7 +1,7 @@
 let Keyboard = window.SimpleKeyboard.default;
 
 let keyboard = new Keyboard({
-  onChange: (input) => onChange(input),
+  //onChange: (input) => onChange(input),
   onKeyPress: (button) => onKeyPress(button),
   mergeDisplay: true,
   layoutName: 'default',
@@ -9,16 +9,19 @@ let keyboard = new Keyboard({
     default: [
       'q w e r t y u i o p',
       'a s d f g h j k l',
-      '{shift} z x c v b n m {backspace}',
-      '{numbers} {space} {ent}',
+      'z x c v b n m',
+      //'{shift} z x c v b n m {backspace}',
+      '',
     ],
-    shift: [
-      'Q W E R T Y U I O P',
-      'A S D F G H J K L',
-      '{shift} Z X C V B N M {backspace}',
-      '{numbers} {space} {ent}',
-    ],
-    numbers: ['1 2 3', '4 5 6', '7 8 9', '{abc} 0 {backspace}'],
+    shift: [],
+    // [
+    //   'Q W E R T Y U I O P',
+    //   'A S D F G H J K L',
+    //   '{shift} Z X C V B N M {backspace}',
+    //   '{numbers} {space} {ent}',
+    // ],
+    numbers: [],
+    //['1 2 3', '4 5 6', '7 8 9', '{abc} 0 {backspace}'],
   },
   display: {
     '{numbers}': '123',
@@ -45,7 +48,7 @@ let keyboard = new Keyboard({
 //   keyboard.setInput(event.target.value);
 // });
 
-console.log(keyboard);
+//console.log(keyboard);
 
 function onChange(input) {
   // document.querySelector('.input').value = input;
